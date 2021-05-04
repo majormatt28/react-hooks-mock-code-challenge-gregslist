79f3b4ee4e126b5ee6713ca4e8510a235b448f9d
Matthew <elijah2895@hotmail.com>
2021-05-04 17:33:29 -0400

    Done

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,20 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ListingCard() {
+function ListingCard({ description, image, location }) {
+  const [isFavorite, setIsFavorite] = useState(false)
+
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={image} alt={"description"} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
+        {isFavorite ? (
+          <button onClick={() => setIsFavorite(false)} className="emoji-button favorite active">★</button>
         ) : (
-          <button className="emoji-button favorite">☆</button>
+          <button onClick={() => setIsFavorite(true)}className="emoji-button favorite">☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
+        <strong>{description}</strong>
+        <span> · {location}</span>
         <button className="emoji-button delete">🗑</button>
       </div>
     </li>
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ListingCard from "./ListingCard";
 // import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+  const [listings, setListings] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then(r => r.json())
+      .then(data => setListings(data))
+  }, [])
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {
+          listings.map(listing => (<ListingCard 
+            key={listing.id} 
+            image={listing.image} 
+            description={listing.description} 
+            location={listing.location}/>))
+        }
       </ul>
     </main>
   );
