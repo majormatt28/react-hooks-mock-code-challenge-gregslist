d56ad0566339c802db378af6ae018d0c64243011
Matthew <elijah2895@hotmail.com>
2021-05-04 22:06:39 -0400

    Good practice run

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([])
+  const [search, setSearch] = useState("")
+
+  function handleDeleteListing(id){
+    const updatedListings = listings.filter(listing => listing.id !== id)
+    
+    setListings(updatedListings)
+  }
+
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+      .then(r => r.json())
+      .then(setListings)
+  }, [])
+
+  const displayedListings = listings.filter(listing => listing.description.toLowerCase().includes(search.toLowerCase()))
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearch={setSearch}/>
+      <ListingsContainer 
+      listings={displayedListings} 
+      onListingDelete={handleDeleteListing}
+      />
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,20 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ listings, onListingDelete }) {
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listings.map(listing => (
+        <ListingCard 
+          key={listing.id} 
+          id={listing.id}
+          description={listing.description}
+          image={listing.image}
+          location={listing.location}
+          onListingDelete={onListingDelete}
+          />
+          ))}
       </ul>
     </main>
   );
